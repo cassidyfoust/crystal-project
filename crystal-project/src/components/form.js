613fc08c354dfc2eed77a9e1f2bc6f114396c781
Cassidy Foust <cassidy.foust@dealerteamwork.com>
2021-11-10 18:09:19 -0600

    all buttons and components in place with no functinality

--- crystal-project/src/components/form.js
+++ crystal-project/src/components/form.js
@@ -1,51 +1,90 @@
+import React, { Component } from 'react';
 import './form.css';
 import Person from './Person.png';
 import Phone from './Phone.png';
 import Email from './Email.png';
 
-function Form() {
-  return (
-    <div class="form">
-        <label for="name" class="label">YOUR NAME</label>
-          <div class="input-group mb-3">
-          <span class="input-group-text" id="basic-addon1">
+class Form extends Component {
+  constructor(props){ 
+    super(props)
+    this.state = {
+      adults: 0,
+      kids: 0,
+      cabins: 0
+      };
+      this.handleAdultsIncrement = this.handleAdultsIncrement.bind(this)
+  } 
+  
+handleAdultsIncrement = (prevState) => {
+  this.setState({...prevState, adults: prevState.adults+1}, () => {
+      console.log(this.state.adults)
+  });
+}
+
+handleAdultsDecrement () {
+  console.log('adults:', this.state.adults)
+  this.setState({...this.state, adults: this.state.adults--})
+};
+
+  render () {
+    return <div className="form">
+        <label className="label">YOUR NAME</label>
+          <div className="input-group mb-3">
+          <span className="input-group-text" id="basic-addon1">
             <img src={Person}/>
           </span>
-          <input type="text" class="form-control" placeholder="Enter your full name" id="name"/>
+          <input type="text" className="form-control" placeholder="Enter your full name" id="name"/>
           </div>
-          <label for="email" class="label">E-MAIL ADDRESS</label>
-          <div class="input-group mb-3">
-          <span class="input-group-text" id="basic-addon1">
+          <label className="label">E-MAIL ADDRESS</label>
+          <div className="input-group mb-3">
+          <span className="input-group-text" id="basic-addon1">
             <img src={Phone}/>
           </span>
-          <input type="text" class="form-control" placeholder="Enter your E-mail address" id="email"/>
+          <input type="text" className="form-control" placeholder="Enter your E-mail address" id="email"/>
 </div>
-<label for="phone" class="label">PHONE NUMBER</label>
-          <div class="input-group mb-3">
-          <span class="input-group-text" id="basic-addon1">
+<label className="label">PHONE NUMBER</label>
+          <div className="input-group mb-3">
+          <span className="input-group-text" id="basic-addon1">
             <img src={Email}/>
           </span>
-          <input type="text" class="form-control" placeholder="Enter your phone number" id="phone"/>
+          <input type="text" className="form-control" placeholder="Enter your phone number" id="phone"/>
 </div>
-<div class="increment-button-wrapper">
-<div class="input-group mb-3 increment-buttons">
-  <button class="btn btn-left btn-outline-secondary" type="button">-</button>
-  <input type="text" class="form-control" placeholder="" aria-label="Example text with two button addons"/>
-  <button class="btn btn-right btn-outline-secondary" type="button">+</button>
+<div className="increment-button-wrapper">
+<div className="input-group mb-3 increment-buttons">
+  <div>
+<label className="label">ADULTS</label>
 </div>
-<div class="input-group mb-3 increment-buttons">
-  <button class="btn btn-left btn-outline-secondary" type="button">-</button>
-  <input type="text" class="form-control" placeholder="" aria-label="Example text with two button addons"/>
-  <button class="btn btn-right btn-outline-secondary" type="button">+</button>
+<div className="increment-display-wrapper">
+  <button className="btn btn-left btn-outline-secondary" type="button" onClick={() => this.handleAdultsDecrement()}>-</button>
+  <input type="text" className="form-control increment-display" value={this.state.adults} disabled={true} id="adults"/>
+  <button className="btn btn-right btn-outline-secondary" type="button" onClick={() => this.handleAdultsIncrement()}>+</button>
+  </div>
 </div>
-<div class="input-group mb-3 increment-buttons">
-  <button class="btn btn-left btn-outline-secondary" type="button">-</button>
-  <input type="text" class="form-control" placeholder="" aria-label="Example text with two button addons"/>
-  <button class="btn btn-right btn-outline-secondary" type="button">+</button>
+<div className="input-group mb-3 increment-buttons">
+  <div>
+<label className="label">KIDS</label>
+</div>
+<div className="increment-display-wrapper">
+  <button className="btn btn-left btn-outline-secondary" type="button">-</button>
+  <input type="text" className="form-control increment-display" value={this.state.kids} disabled={true} id="kids"/>
+  <button className="btn btn-right btn-outline-secondary" type="button">+</button>
+  </div>
+</div>
+<div className="input-group mb-3 increment-buttons">
+  <div>
+<label className="label">CABIN ROOMS</label>
+</div>
+<div className="increment-display-wrapper">
+  <button className="btn btn-left btn-outline-secondary" type="button" onClick={() => this.handleDecrement("cabins")}>-</button>
+  <input type="text" className="form-control increment-display" value={this.state.cabins} disabled={true} id="cabins"/>
+  <button className="btn btn-right btn-outline-secondary" type="button">+</button>
+  </div>
 </div>
 </div>
+<button type="button" class="btn btn-primary submit-btn">COMPLETE YOUR BOOKING</button>
+
     </div>
-  );
+  }
 }
 
 export default Form;
